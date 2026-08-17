c4a135a7df7f982c573d4ceddcf5bd8d83128e93
F3lia5 <thefoolasking@gmail.com>
2026-08-17 18:51:11 +0300

    Update models.py

---
+++ src/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from enum import Enum
+from dataclasses import dataclass
+
+class harcama_kategorisi(Enum):
+    YIYECEK: 1
+    ULASIM: 2
+    EGLENCE: 3
+    MARKET: 4
+    EV: 5
+    ALISVERIS: 6
+    SAGLIK: 7
+    EGITIM: 8
+    DIGER: 9
+
+@dataclass
+class Expense():
+    harcama_tutari: float/int
+    harcama_kategorisi: Enum/str
+    notlar: str
+    harcama_tarihi_dt: datetime
+    @property
+    def harcama_tarihi_dt(str) -> str:
+        '''ilk defa datetime'i boyle deniyom ins tekte calisir'''
+        return self.harcama_tarihi_dt.isoformat()
+
+#try koncak bunlara hata ayiklamak icin (sadece elzem olanlar; eger harcama tutarina veya kategoriye secenekli buton koyabilirsem gerek yok)
