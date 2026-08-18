a2c08538c284ac6e419e035ee34f7d466aca163a
F3lia5 <thefoolasking@gmail.com>
2026-08-18 13:56:24 +0300

    Storage and Models update

--- src/storage.py
+++ src/storage.py
@@ -1,21 +1,20 @@
-from dataclasses import asdict
 import json
 from models import Expense, ornek_deney
 import os
 
-veri = asdict(ornek_deney)
-veri['harcama_tarihi_dt'] = veri['harcama_tarihi_dt'].isoformat()
-veri['harcama_kategorisi'] = veri['harcama_kategorisi'].name
+def harcama_yukle():
+    if os.path.exists('harcama.json'):
+        with open('harcama.json', 'r') as f:
+            data = json.load(f)
+            return data
+    return []
 
-dosya_varmi = os.path.exists('harcama.json')
+def harcama_ekle(veri):
+    veri['harcama_tarihi_dt'] = veri['harcama_tarihi_dt'].isoformat()
+    veri['harcama_kategorisi'] = veri['harcama_kategorisi'].name
 
-if dosya_varmi:
-    with open('harcama.json', 'r') as f:
-        data = json.load(f)
-        print(data)
-else:
-    data = []
+    data = harcama_yukle()
 
-data.append(veri)
-with open('harcama.json', 'w') as f:
-    json.dump(data, f)
+    data.append(veri)
+    with open('harcama.json', 'w') as f:
+        json.dump(data, f)
