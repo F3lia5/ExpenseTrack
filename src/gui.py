bd7afa90448925248281a3d1e47cefac6babb8e6
F3lia5 <thefoolasking@gmail.com>
2026-08-20 15:38:23 +0300

    New version gui.py v0.02

--- src/gui.py
+++ src/gui.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import IntVar
 from models import HarcamaKategorisi
 
 def kategori_sec():
     secilen = kategori_var.get()
-    messagebox.showinfo("Kategori", f"Seçilen kategori: {secilen}")
-
+    messagebox.showinfo("Kategori", f"Seçilen kategori: {secilen} \n Tutar: {scale.get()}")
 
 root = tk.Tk()
 root.title("Expense Tracker")
@@ -18,7 +18,8 @@ for secilenkategori in HarcamaKategorisi:
     giris = tk.Radiobutton(root, text=secilenkategori.name, variable=kategori_var, value=secilenkategori.name)
     giris.pack()
 
-
+scale = tk.Scale(root, from_=0, to=10000, orient=tk.HORIZONTAL, resolution=1, variable=IntVar())
+scale.pack()
 
 kategori_sec_button = tk.Button(root, text="Kategori Seç", command=kategori_sec)
 kategori_sec_button.pack()
